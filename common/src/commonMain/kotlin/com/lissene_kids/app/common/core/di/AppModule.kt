22b9f77c99235162d015a21011adeb690295db28
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-06-07 13:05:35 +0100

    Some Changes

--- common/src/commonMain/kotlin/com/lissene_kids/app/common/core/di/AppModule.kt
+++ common/src/commonMain/kotlin/com/lissene_kids/app/common/core/di/AppModule.kt
@@ -3,7 +3,6 @@ package com.lissene_kids.app.common.core.di
 
 import com.lissene_kids.app.common.auth.di.authModule
 import com.lissene_kids.app.common.core.network.networkModule
-import com.lissene_kids.app.common.home.di.homeModule
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
@@ -14,7 +13,6 @@ fun initKoin(appDeclaration: KoinAppDeclaration) = run {
             networkModule,
 
             authModule,
-            homeModule,
 
         )
     }
